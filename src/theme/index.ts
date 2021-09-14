d35dbe5bd3d23677b569d7a4fbaa6f9bb7ae18a7
Biraj Pokharel <54663280+Birajpokharel11@users.noreply.github.com>
2021-09-14 16:59:29 +0545

    Updated Theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,9 @@
 import { createTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 
+import overrides from './overrides';
+import typography from './typography';
+
 // Create a theme instance.
 const theme = createTheme({
   palette: {
@@ -16,7 +19,9 @@ const theme = createTheme({
     background: {
       default: '#fff'
     }
-  }
+  },
+  overrides,
+  typography
 });
 
 export default theme;

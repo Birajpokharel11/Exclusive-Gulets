5e0a6e43b9bf0e1cab04b61e188108da8e3d445e
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-09-14 17:02:04 +0545

    updated theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,27 +1,14 @@
 import { createTheme } from '@material-ui/core/styles';
-import red from '@material-ui/core/colors/red';
 
-import overrides from './overrides';
+import palette from './palette';
 import typography from './typography';
+import overrides from './overrides';
 
 // Create a theme instance.
 const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#091527'
-    },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    background: {
-      default: '#fff'
-    }
-  },
-  overrides,
-  typography
+  palette,
+  typography,
+  overrides
 });
 
 export default theme;

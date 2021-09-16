846067b50d35b4a7a99152cc2b32cfa33f41e1ca
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-09-16 11:35:30 +0545

    added mock data

---
+++ src/views/Home/_homeSliderMock.js
@@ -0,0 +1,78 @@
+import img from 'assets/images/Home/_slider.jpg';
+export default [
+  {
+    title: 'Mare Nostrum',
+    location: 'Turkey',
+    description: 'Dolce Mare is among the very best in luxury charter gulets, she is 36 meters in length' +
+      ' with 6 deluxe en suite cabins to comfortably accommodate up to 12 guests. ',
+    path: '/yachts/mare-nostrum',
+    img: img,
+    review: {
+      author: 'Yasmin, Israel',
+      message: '“Thank you all so so so much for all of your help and patience with getting' +
+        ' through this kosher week. My family had the best trip of their lives so I take my hat off to you all.' +
+        ' Please pass on my sincere thanks also to Captain Askin and his team who I know showed patience and' +
+        ' professionalism throughout what was no doubt a very challenging week.”'
+    }
+  },
+  {
+    title: 'Mare Nostrum',
+    location: 'Turkey',
+    description: 'Dolce Mare is among the very best in luxury charter gulets, she is 36 meters in length' +
+      ' with 6 deluxe en suite cabins to comfortably accommodate up to 12 guests. ',
+    path: '/yachts/mare-nostrum',
+    img: img,
+    review: {
+      author: 'Yasmin, Israel',
+      message: '“Thank you all so so so much for all of your help and patience with getting' +
+        ' through this kosher week. My family had the best trip of their lives so I take my hat off to you all.' +
+        ' Please pass on my sincere thanks also to Captain Askin and his team who I know showed patience and' +
+        ' professionalism throughout what was no doubt a very challenging week.”'
+    }
+  },
+  {
+    title: 'Mare Nostrum',
+    location: 'Turkey',
+    description: 'Dolce Mare is among the very best in luxury charter gulets, she is 36 meters in length' +
+      ' with 6 deluxe en suite cabins to comfortably accommodate up to 12 guests. ',
+    path: '/yachts/mare-nostrum',
+    img: img,
+    review: {
+      author: 'Yasmin, Israel',
+      message: '“Thank you all so so so much for all of your help and patience with getting' +
+        ' through this kosher week. My family had the best trip of their lives so I take my hat off to you all.' +
+        ' Please pass on my sincere thanks also to Captain Askin and his team who I know showed patience and' +
+        ' professionalism throughout what was no doubt a very challenging week.”'
+    }
+  },
+  {
+    title: 'Mare Nostrum',
+    location: 'Turkey',
+    description: 'Dolce Mare is among the very best in luxury charter gulets, she is 36 meters in length' +
+      ' with 6 deluxe en suite cabins to comfortably accommodate up to 12 guests. ',
+    path: '/yachts/mare-nostrum',
+    img: img,
+    review: {
+      author: 'Yasmin, Israel',
+      message: '“Thank you all so so so much for all of your help and patience with getting' +
+        ' through this kosher week. My family had the best trip of their lives so I take my hat off to you all.' +
+        ' Please pass on my sincere thanks also to Captain Askin and his team who I know showed patience and' +
+        ' professionalism throughout what was no doubt a very challenging week.”'
+    }
+  },
+  {
+    title: 'Mare Nostrum',
+    location: 'Turkey',
+    description: 'Dolce Mare is among the very best in luxury charter gulets, she is 36 meters in length' +
+      ' with 6 deluxe en suite cabins to comfortably accommodate up to 12 guests. ',
+    path: '/yachts/mare-nostrum',
+    img: img,
+    review: {
+      author: 'Yasmin, Israel',
+      message: '“Thank you all so so so much for all of your help and patience with getting' +
+        ' through this kosher week. My family had the best trip of their lives so I take my hat off to you all.' +
+        ' Please pass on my sincere thanks also to Captain Askin and his team who I know showed patience and' +
+        ' professionalism throughout what was no doubt a very challenging week.”'
+    }
+  }
+]

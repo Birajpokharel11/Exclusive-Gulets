b5ff3ecae430f8b54a80d4ec7420835654f00fd5
Biraj Pokharel <54663280+Birajpokharel11@users.noreply.github.com>
2021-09-21 12:22:13 +0545

    pagination mobile responsive

--- src/views/Yachts/components/Gallery/PaginationComponents.tsx
+++ src/views/Yachts/components/Gallery/PaginationComponents.tsx
@@ -5,29 +5,45 @@ import {
   withStyles,
   Theme
 } from '@material-ui/core/styles';
-import { Box, Container, Typography } from '@material-ui/core';
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+  useMediaQuery
+} from '@material-ui/core';
 import Pagination from './Pagination';
-
+import { useTheme } from '@material-ui/styles';
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     Box: { width: '100%', background: '#091527' },
     Container: {
       display: 'flex',
       padding: '66px 100px 80px 100px',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
+      [theme.breakpoints.down('sm')]: {
+        padding: '66px 10px 80px 10px'
+      },
+      [theme.breakpoints.down('xs')]: {
+        display: 'flex',
+        flexDirection: 'column'
+      }
     }
   })
 );
 
 export default function PaginationSection() {
   const classes = useStyles();
-
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.down('xs'));
   const [offers, setOffers] = React.useState(true);
 
   return (
     <Box className={classes.Box}>
       <Container maxWidth="xl" className={classes.Container}>
-        <Typography color="secondary">120 yachts are listed</Typography>
+        <Typography align={matches ? 'center' : ''} color="secondary">
+          120 yachts are listed
+        </Typography>
         <Pagination />
       </Container>
     </Box>

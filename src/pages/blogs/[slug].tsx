64b125afbb709aadb420177ccddc1be3d49c6fc4
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-09-25 09:58:26 +0545

    added section

--- src/pages/blogs/[slug].tsx
+++ src/pages/blogs/[slug].tsx
@@ -6,7 +6,7 @@ import React from 'react';
 
 import WithLayout from '@components/WithLayout';
 import Main from '@layouts/Main';
-import BlogsDetails from '@views/BlogsDetails';
+import BlogsDetails from '@views/Blogs/Details';
 
 export default function NewsBlogs() {
   return <WithLayout component={BlogsDetails} layout={Main} />;
---
+++ src/pages/experiences/[slug].tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import WithLayout from '@components/WithLayout';
+import Main from '@layouts/Main';
+import ExperiencesDetails from '@views/Experiences/Details';
+
+export default function NewsBlogs() {
+  return <WithLayout component={DestinationsDetails} layout={Main} />;
+}

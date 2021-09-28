9626adb9604bd7da0cab0bd5cd5a662495aab30d
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-09-28 18:25:13 +0545

    breadcrumb cleanuo

--- src/components/BreadCrumbs/BreadCrumbs.tsx
+++ src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import clsx from 'clsx';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Link from '@material-ui/core/Link';
 import { IconButton } from '@material-ui/core';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
-import clsx from 'clsx';
-function handleClick(event) {
-  event.preventDefault();
-  console.info('You clicked a breadcrumb.');
-}
+
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: { position: 'relative', top: '100px', left: '50px' },
     breadCrumbs: { fontWeight: 400 }
   })
 );
+
 export default function BreadCrumbs() {
   const classes = useStyles();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    console.info('You clicked a breadcrumb.');
+  };
+
   return (
     <Breadcrumbs aria-label="breadcrumb" className={classes.root}>
       <Link color="secondary" href="/" onClick={handleClick}>

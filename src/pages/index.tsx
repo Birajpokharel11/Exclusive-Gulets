316000058103ec4ebc1f943e501ee11dd3c1b780
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-09-29 19:31:35 +0545

    uncommented getInitialProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,17 +35,17 @@ export const getServerSideProps = wrapper.getServerSideProps(
   }
 );
 
-// Home.getInitialProps = ({ req }: NextPageContext) => {
-//   let isIOS = false;
-//   let userAgent;
-//   if (req) {
-//     // if you are on the server and you get a 'req' property from your context
-//     userAgent = req.headers['user-agent']; // get the user-agent from the headers
-//   } else {
-//     userAgent = navigator.userAgent; // if you are on the client you can access the navigator from the window object
-//     isIOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
-//   }
-//   return {
-//     isIOS
-//   };
-// };
+Home.getInitialProps = ({ req }: NextPageContext) => {
+  let isIOS = false;
+  let userAgent;
+  if (req) {
+    // if you are on the server and you get a 'req' property from your context
+    userAgent = req.headers['user-agent']; // get the user-agent from the headers
+  } else {
+    userAgent = navigator.userAgent; // if you are on the client you can access the navigator from the window object
+    isIOS = /(iPad|iPhone|iPod)/g.test(navigator.userAgent);
+  }
+  return {
+    isIOS
+  };
+};

7034f73d299647c838a90a54ba1185d906def590
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-10-01 09:02:23 +0545

    updated yachts/offer

--- src/pages/yachts/offers.tsx
+++ src/pages/yachts/offers.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { END } from 'redux-saga';
+import { wrapper } from '@store/index';
+import { fetchYachtsStart } from '@store/yachts/yachts.actions';
+import { fetchRandomDestinationStart } from '@store/destination/destination.actions';
+import { fetchExperiencesStart } from '@store/experiences/experiences.actions';
+
 import WithLayout from '@components/WithLayout';
 import Main from '@layouts/Main';
-import Yatchs from '@views/Yachts';
+import YachtPage from '@views/Yachts';
 
 export default function Yatch() {
-  return <WithLayout component={Yatchs} layout={Main} />;
+  return <WithLayout component={YachtPage} layout={Main} />;
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    store.dispatch(fetchYachtsStart());
+    store.dispatch(fetchRandomDestinationStart());
+    store.dispatch(fetchExperiencesStart());
+    store.dispatch(END);
+
+    await store.sagaTask?.toPromise();
+    const myStore = store.getState();
+    const yacht = myStore.yacht;
+
+    return {
+      props: { yacht }
+    };
+  }
+);

e40f37f589eac755e4d486a68200d87f73b9c48d
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-10-06 08:09:29 +0545

    added formik to enquiry form

--- src/theme/overrides/MuiSelect.ts
+++ src/theme/overrides/MuiSelect.ts
@@ -2,12 +2,7 @@ import { StyleRules } from '@material-ui/core/styles/withStyles';
 import { SelectClassKey } from '@material-ui/core/Select';
 
 const MuiSelect: Partial<StyleRules<SelectClassKey, {}>> = {
-  root: {
-    background: '#FFFFFF',
-    '& :focus': {
-      background: '#FFFFFF'
-    }
-  }
+  root: {}
 };
 
 export default MuiSelect;
--- src/theme/overrides/MuiTextField.ts
+++ src/theme/overrides/MuiTextField.ts
@@ -3,11 +3,7 @@ import { TextFieldClassKey } from '@material-ui/core/TextField';
 
 const MuiTextField: Partial<StyleRules<TextFieldClassKey, {}>> = {
   root: {
-    margin: '10px 0',
-    background: '#FFFFFF',
-    '& :focus': {
-      background: '#FFFFFF'
-    }
+    margin: '10px 0'
   }
 };
 

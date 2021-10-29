1d5a033bdd96134386b867f9f19a849b101a0c0d
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-10-29 13:54:48 +0545

    fixed import issue

---
+++ src/components/MakeLink.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Link from 'next/link';
+
+const MakeLink = (props) => {
+  const { href, children, ...rest } = props;
+  const child = React.cloneElement(children, rest);
+  return <Link href={href}>{child}</Link>;
+};
+
+export default MakeLink;

d76f14cca6b324411a43a6c62c72fffcdc30a83a
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-11-02 19:18:53 +0545

    check domain

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -1,4 +1,4 @@
-export function getTenantDomain(host) {
+export async function getTenantDomain(host, request = false) {
   let isDev = host.includes('localhost');
   let splitHost = host.split('.');
 
@@ -6,6 +6,42 @@ export function getTenantDomain(host) {
     let page = splitHost[0];
     if (page === 'www' || page === 'exclusivegulets') {
       return null;
+    }
+
+    if (request) {
+      let res = await fetch(
+        'http://3.109.159.153:8080/public/validateUserEmailAndBrokerSite',
+        {
+          method: 'POST', // or 'PUT'
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            site: page
+          })
+        }
+      );
+
+      if (res.status === 200) {
+        const result: any = await res.json();
+        const data = result?.detail?.data;
+        if (data.isValidSite) {
+          return null;
+        }
+        return page;
+
+        // return { html, allowEdit, editLink: `${href}?edit=${token}` };
+      }
+
+      // if (res.status === 404) {
+      //   let { html, token } = await res.json();
+      //   return { html, editLink: `${href}?edit=${token}` };
+      // }
+
+      // if (!res.ok && res.status !== 404) {
+      //   let { stack, message } = await res.json();
+      //   return { errorCode: res.status, stack, message };
+      // }
     }
 
     return page;

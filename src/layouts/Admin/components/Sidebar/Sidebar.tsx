f9f9dc1b759792b0bec55bc6ff6dc7cda0f16063
kiran <maharjank595.gmail.com>
2021-11-11 11:03:53 +0545

    dashboard link changed

--- src/layouts/Admin/components/Sidebar/Sidebar.tsx
+++ src/layouts/Admin/components/Sidebar/Sidebar.tsx
@@ -46,7 +46,7 @@ const Sidebar = (props) => {
   const pages = [
     {
       title: 'Dashboard',
-      href: '/dashboard',
+      href: '/manage/dashboard',
       icon: <DashboardIcon />
     },
     {

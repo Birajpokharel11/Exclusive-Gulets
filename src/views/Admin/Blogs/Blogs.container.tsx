7c372c4e4eef06b6eda774ce4d9a359ca5b8cc86
Nikhil Shrestha <nikhil.shrestha1995@gmail.com>
2021-11-19 16:59:03 +0545

    fixed issue blog

--- src/views/Admin/Blogs/Blogs.container.tsx
+++ src/views/Admin/Blogs/Blogs.container.tsx
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { createPostStart, fetchPostsStart } from '@store/posts/posts.actions';
+
 const mapStateToProps = (state, props) => ({
   destination: state.destination,
   posts: state.posts,
@@ -7,7 +8,15 @@ const mapStateToProps = (state, props) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  onCreatePostStart: (formData) => dispatch(createPostStart(formData)),
+  onCreatePostStart: (
+    formData,
+    mainSelectedFile,
+    sideSelectedFile,
+    domainName
+  ) =>
+    dispatch(
+      createPostStart(formData, mainSelectedFile, sideSelectedFile, domainName)
+    ),
   onFetchPostsStart: (id) => dispatch(fetchPostsStart(id))
 });
 

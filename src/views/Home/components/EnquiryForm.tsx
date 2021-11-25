ce6ce8ee602191a8aaf93a2361667edd4babdde6
BirajPokharel <buraj.sainik@gmail.com>
2021-11-25 02:29:18 -0500

    Enquiry form error solved

--- src/views/Home/components/EnquiryForm.tsx
+++ src/views/Home/components/EnquiryForm.tsx
@@ -7,12 +7,15 @@ import {
   Typography,
   Button,
   Select,
-  MenuItem
+  MenuItem,
+  TextField,
+  FormControl,
+  InputLabel,
+  FormHelperText
 } from '@material-ui/core';
 import { useTheme } from '@material-ui/core/styles';
 
-import { Formik, Field, Form } from 'formik';
-import { TextField } from 'formik-material-ui';
+import { useFormik } from 'formik';
 import * as Yup from 'yup';
 
 import countryList from '@mocks/country_code.json';
@@ -49,14 +52,32 @@ const useStyles = makeStyles((theme) =>
   })
 );
 
+const validationSchema = Yup.object({
+  title: Yup.string().required('This field is required'),
+  name: Yup.string().required('This field is required'),
+  email: Yup.string().required('This field is required'),
+  country: Yup.string().required('This field is required'),
+  phonenumber: Yup.string().required('This field is required'),
+  comments: Yup.string().required('This field is required')
+});
 export default function EnquiryForm() {
   const classes = useStyles();
 
-  const handleSubmit = (values) => {
-    console.log('values', values);
-  };
   const theme = useTheme();
-
+  const formik = useFormik({
+    initialValues: {
+      title: '',
+      name: '',
+      email: '',
+      country: '',
+      phonenumber: '',
+      comments: ''
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    }
+  });
   return (
     <Box component="section" className={classes.root}>
       <Container maxWidth="md">
@@ -91,148 +112,216 @@ export default function EnquiryForm() {
       </Container>
       <div style={{ paddingTop: '59px' }} />
       <Container>
-        <Formik
-          initialValues={{
-            title: '',
-            name: '',
-            email: '',
-            country: '',
-            phonenumber: '',
-            comments: ''
-          }}
-          validationSchema={Yup.object({
-            title: Yup.string().required('This field is required'),
-            name: Yup.string().required('This field is required'),
-            email: Yup.string().required('This field is required'),
-            country: Yup.string().required('This field is required'),
-            phonenumber: Yup.string().required('This field is required'),
-            comments: Yup.string().required('This field is required')
-          })}
-          onSubmit={handleSubmit}
-        >
-          <Form>
-            <Grid container spacing={3}>
-              <Grid item container md={6} sm={12} xs={12} spacing={2}>
-                <Grid item xs={4}>
-                  <Field
-                    component={Select}
-                    name="title"
+        <form onSubmit={formik.handleSubmit}>
+          <Grid container spacing={3}>
+            <Grid item container md={6} sm={12} xs={12} spacing={2}>
+              <Grid item xs={4}>
+                <FormControl
+                  variant="outlined"
+                  className={classes.formControl}
+                  error={formik.touched.title && Boolean(formik.errors.title)}
+                >
+                  <InputLabel id="demo-simple-select-outlined-label">
+                    Title
+                  </InputLabel>
+                  <Select
                     id="title"
-                    label="Title"
-                    variant="outlined"
-                    style={{ marginTop: '10px' }}
-                    InputLabelProps={{
-                      style: { color: '#091527' }
-                    }}
-                    IconComponent={() => <ExpandMoreIcon />}
-                    fullWidth
-                  >
-                    <MenuItem value="Mr.">Mr.</MenuItem>
-                    <MenuItem value="Mrs.">Mrs.</MenuItem>
-                    <MenuItem value="Ms.">Ms.</MenuItem>
-                    <MenuItem value="Miss.">Miss.</MenuItem>
-                    <MenuItem value="Mx.">Mx.</MenuItem>
-                  </Field>
-                </Grid>
-                <Grid item xs={8}>
-                  <Field
-                    component={TextField}
-                    id="name"
-                    name="name"
-                    label="Full Name"
-                    variant="outlined"
-                    fullWidth
-                    InputLabelProps={{
-                      style: { color: '#091527' }
-                    }}
-                    InputProps={{ notched: false }}
-                    placeholder="Write your name"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <Field
-                    component={TextField}
-                    id="email"
-                    name="email"
-                    InputLabelProps={{
-                      style: { color: '#091527' }
-                    }}
-                    label="Email"
-                    variant="outlined"
-                    fullWidth
-                    InputProps={{ notched: false }}
-                  />
-                </Grid>
-                <Grid item xs={3}>
-                  <Field
-                    component={Select}
-                    id="country"
-                    name="country"
-                    InputLabelProps={{
-                      style: { color: '#091527' }
-                    }}
-                    style={{ marginTop: '10px' }}
+                    name="title"
+                    style={{ marginTop: '10px', width: '190px' }}
                     IconComponent={() => <ExpandMoreIcon />}
                     label="Select"
-                    variant="outlined"
                     fullWidth
-                    SelectProps={{
-                      MenuProps: menuProps
-                    }}
+                    MenuProps={menuProps}
                     InputProps={{ notched: false }}
+                    value={formik.values.title}
+                    onChange={formik.handleChange}
+                    error={formik.touched.title && Boolean(formik.errors.title)}
+                    helperText={formik.touched.title && formik.errors.title}
+                  >
+                    <MenuItem value="">
+                      <em>None</em>
+                    </MenuItem>
+                    <MenuItem value={10}>Mr.</MenuItem>
+                    <MenuItem value={20}>Ms.</MenuItem>
+                    <MenuItem value={30}>Mrs.</MenuItem>
+                    <MenuItem value={30}>Miss.</MenuItem>
+                    <MenuItem value={30}>Mx.</MenuItem>
+                  </Select>
+                  {formik.touched.title && formik.errors.title && (
+                    <FormHelperText>{formik.errors.title}</FormHelperText>
+                  )}
+                </FormControl>
+
+                {/* <Field
+                  component={Select}
+                  name="title"
+                  id="title"
+                  label="Title"
+                  variant="outlined"
+                  style={{ marginTop: '10px' }}
+                  InputLabelProps={{
+                    style: { color: '#091527' }
+                  }}
+                  IconComponent={() => <ExpandMoreIcon />}
+                  fullWidth
+                >
+                  <MenuItem value="Mr.">Mr.</MenuItem>
+                  <MenuItem value="Mrs.">Mrs.</MenuItem>
+                  <MenuItem value="Ms.">Ms.</MenuItem>
+                  <MenuItem value="Miss.">Miss.</MenuItem>
+                  <MenuItem value="Mx.">Mx.</MenuItem>
+                </Field> */}
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  id="name"
+                  name="name"
+                  label="Full Name"
+                  variant="outlined"
+                  fullWidth
+                  value={formik.values.name}
+                  onChange={formik.handleChange}
+                  error={formik.touched.name && Boolean(formik.errors.name)}
+                  helperText={formik.touched.name && formik.errors.name}
+                  // InputLabelProps={{
+                  //   style: { color: '#091527' }
+                  // }}
+                  // InputProps={{ notched: false }}
+                  placeholder="Write your name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  id="email"
+                  name="email"
+                  label="Email"
+                  variant="outlined"
+                  fullWidth
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  error={formik.touched.email && Boolean(formik.errors.email)}
+                  // InputProps={{ notched: false }}
+                  // InputLabelProps={{
+                  //   style: { color: '#091527' }
+                  // }}
+                />
+              </Grid>
+              <Grid item xs={4}>
+                <FormControl
+                  variant="outlined"
+                  error={
+                    formik.touched.country && Boolean(formik.errors.country)
+                  }
+                >
+                  <InputLabel id="demo-simple-select-outlined-label">
+                    Country
+                  </InputLabel>
+                  <Select
+                    id="country"
+                    name="country"
+                    style={{ marginTop: '10px', width: '190px' }}
+                    IconComponent={() => <ExpandMoreIcon />}
+                    label="Select"
+                    fullWidth
+                    MenuProps={menuProps}
+                    InputProps={{ notched: false }}
+                    value={formik.values.country}
+                    onChange={formik.handleChange}
+                    error={
+                      formik.touched.country && Boolean(formik.errors.country)
+                    }
+                    helperText={formik.touched.country && formik.errors.country}
                   >
                     {countryList.map((country) => (
                       <MenuItem key={country.label} value={country.value}>
                         {country.label}
                       </MenuItem>
                     ))}
-                  </Field>
-                </Grid>
-                <Grid item xs={9}>
-                  <Field
-                    component={TextField}
-                    id="phonenumber"
-                    name="phonenumber"
-                    label="Mobile Phone"
-                    variant="outlined"
-                    fullWidth
-                    InputLabelProps={{
-                      style: { color: '#091527' }
-                    }}
-                    InputProps={{ notched: false }}
-                  />
-                </Grid>
-              </Grid>
-              <Grid item md={6} sm={12} xs={12}>
-                <Field
-                  component={TextField}
-                  id="comments"
-                  name="comments"
-                  label="Your Comments"
-                  style={{ color: 'green' }}
-                  multiline
+                  </Select>
+                  {formik.touched.country && formik.errors.country && (
+                    <FormHelperText>{formik.errors.country}</FormHelperText>
+                  )}
+                </FormControl>
+                {/* <Field
+                  component={Select}
+                  id="country"
+                  name="country"
                   InputLabelProps={{
                     style: { color: '#091527' }
                   }}
-                  rows={11}
+                  style={{ marginTop: '10px' }}
+                  IconComponent={() => <ExpandMoreIcon />}
+                  label="Select"
                   variant="outlined"
                   fullWidth
+                  SelectProps={{
+                    MenuProps: menuProps
+                  }}
                   InputProps={{ notched: false }}
+                >
+                  {countryList.map((country) => (
+                    <MenuItem key={country.label} value={country.value}>
+                      {country.label}
+                    </MenuItem>
+                  ))}
+                </Field> */}
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  id="phonenumber"
+                  name="phonenumber"
+                  label="Mobile Phone"
+                  variant="outlined"
+                  fullWidth
+                  value={formik.values.phonenumber}
+                  onChange={formik.handleChange}
+                  error={
+                    formik.touched.phonenumber &&
+                    Boolean(formik.errors.phonenumber)
+                  }
+                  helperText={
+                    formik.touched.phonenumber && formik.errors.phonenumber
+                  }
+                  // InputLabelProps={{
+                  //   style: { color: '#091527' }
+                  // }}
+                  // InputProps={{ notched: false }}
                 />
               </Grid>
-              <Grid item container justifyContent="center">
-                <Button
-                  variant="contained"
-                  size="large"
-                  type="submit"
-                  className={classes.buttonStyle}
-                >
-                  Enquire
-                </Button>
-              </Grid>
             </Grid>
-          </Form>
-        </Formik>
+            <Grid item md={6} sm={12} xs={12}>
+              <TextField
+                id="comments"
+                name="comments"
+                label="Your Comments"
+                multiline
+                rows={11}
+                variant="outlined"
+                fullWidth
+                // InputProps={{ notched: false }}
+                // InputLabelProps={{
+                //   style: { color: '#091527' }
+                // }}
+                value={formik.values.comments}
+                onChange={formik.handleChange}
+                error={
+                  formik.touched.comments && Boolean(formik.errors.comments)
+                }
+                helperText={formik.touched.comments && formik.errors.comments}
+              />
+            </Grid>
+            <Grid item container justifyContent="center">
+              <Button
+                variant="contained"
+                size="large"
+                type="submit"
+                className={classes.buttonStyle}
+              >
+                Enquire
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
       </Container>
     </Box>
   );

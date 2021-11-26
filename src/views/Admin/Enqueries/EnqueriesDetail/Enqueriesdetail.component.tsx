85705da725d7397ef2ae1aeae57db33245574d4c
BirajPokharel <buraj.sainik@gmail.com>
2021-11-26 09:36:36 -0500

    Enquiry details

--- src/views/Admin/Enqueries/EnqueriesDetail/Enqueriesdetail.component.tsx
+++ src/views/Admin/Enqueries/EnqueriesDetail/Enqueriesdetail.component.tsx
@@ -159,244 +159,113 @@ const EnquiryForm = ({ onFetchEnqueriesByIdStart, enquiry }: Props) => {
   const matches = useMediaQuery(theme.breakpoints.up('xs'));
   return (
     <Box component="section" className={classes.root}>
-      <Container maxWidth="md">
-        <Grid
-          container
-          justifyContent="center"
-          alignItems="center"
-          direction="column"
-          spacing={2}
-        >
-          <Grid item xs={12}>
-            <Typography variant="h2" color="textPrimary" align="center">
-              We can save you time & money.
-            </Typography>
-          </Grid>
-        </Grid>
+      <Container maxWidth="lg">
+        <Typography variant="h2" color="textPrimary" align="left">
+          Individual Enqueries
+        </Typography>
       </Container>
-      {matches && <div style={{ paddingBottom: '91px' }} />}
-      <div style={{ paddingTop: '59px' }} />
-      <Container className={classes.Container}>
-        <form onSubmit={formik.handleSubmit}>
-          <Grid container spacing={5}>
-            <Grid item container md={6} sm={6} xs={12} spacing={2}>
-              <Grid item md={5} lg={4} xs={4} sm="auto">
-                <FormControl
-                  variant="outlined"
-                  error={formik.touched.title && Boolean(formik.errors.title)}
-                >
-                  <InputLabel
-                    id="demo-simple-select-outlined-label"
-                    style={{ color: '#091527', margin: '12px 0' }}
-                  >
-                    Title
-                  </InputLabel>
-                  <Select
-                    id="title"
-                    name="title"
-                    className={classes.SelectTitle}
-                    label="Select"
-                    fullWidth
-                    MenuProps={menuProps}
-                    value={formik.values.title}
-                    onChange={formik.handleChange}
-                  >
-                    <MenuItem value="">
-                      <em>None</em>
-                    </MenuItem>
-                    <MenuItem value={10}>Mr.</MenuItem>
-                    <MenuItem value={20}>Ms.</MenuItem>
-                    <MenuItem value={30}>Mrs.</MenuItem>
-                    <MenuItem value={30}>Miss.</MenuItem>
-                    <MenuItem value={30}>Mx.</MenuItem>
-                  </Select>
-                  {formik.touched.title && formik.errors.title && (
-                    <FormHelperText>{formik.errors.title}</FormHelperText>
-                  )}
-                </FormControl>
 
-                {/* <Field
-                  component={Select}
-                  name="title"
-                  id="title"
-                  label="Title"
-                  variant="outlined"
-                  style={{ marginTop: '10px' }}
-                  InputLabelProps={{
-                    style: { color: '#091527' }
-                  }}
-                  IconComponent={() => <ExpandMoreIcon />}
-                  fullWidth
-                >
-                  <MenuItem value="Mr.">Mr.</MenuItem>
-                  <MenuItem value="Mrs.">Mrs.</MenuItem>
-                  <MenuItem value="Ms.">Ms.</MenuItem>
-                  <MenuItem value="Miss.">Miss.</MenuItem>
-                  <MenuItem value="Mx.">Mx.</MenuItem>
-                </Field> */}
-              </Grid>
-              <Grid item md={7} lg={8} xs={8} sm={6}>
-                <TextField
-                  id="name"
-                  name="name"
-                  label="Full Name"
-                  variant="outlined"
-                  fullWidth
-                  className={classes.TextFieldName}
-                  value={formik.values.name}
-                  onChange={formik.handleChange}
-                  error={formik.touched.name && Boolean(formik.errors.name)}
-                  InputLabelProps={{
-                    style: { color: '#091527' }
-                  }}
-                  // InputProps={{ notched: false }}
-                />
-                {formik.touched.name && formik.errors.name && (
-                  <FormHelperText style={{ color: 'red' }}>
-                    {formik.errors.name}
-                  </FormHelperText>
-                )}
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  id="email"
-                  name="email"
-                  label="Email"
-                  variant="outlined"
-                  fullWidth
-                  className={classes.TextFieldEmail}
-                  value={formik.values.email}
-                  onChange={formik.handleChange}
-                  error={formik.touched.email && Boolean(formik.errors.email)}
-                  InputLabelProps={{
-                    style: { color: '#091527' }
-                  }}
-                />{' '}
-                {formik.touched.email && formik.errors.email && (
-                  <FormHelperText style={{ color: 'red' }}>
-                    {formik.errors.email}
-                  </FormHelperText>
-                )}
-              </Grid>
-              <Grid item md={5} lg={4} xs={6} sm="auto">
-                <FormControl
-                  variant="outlined"
-                  error={
-                    formik.touched.country && Boolean(formik.errors.country)
-                  }
-                >
-                  <InputLabel
-                    id="demo-simple-select-outlined-label"
-                    style={{ color: '#091527', margin: '12px 0' }}
-                  >
-                    Country
-                  </InputLabel>
-                  <Select
-                    id="country"
-                    name="country"
-                    className={classes.SelectCountry}
-                    label="Select"
-                    fullWidth
-                    MenuProps={menuProps}
-                    value={formik.values.country}
-                    onChange={formik.handleChange}
-                    error={
-                      formik.touched.country && Boolean(formik.errors.country)
-                    }
-                  >
-                    {countryList.map((country) => (
-                      <MenuItem key={country.label} value={country.label}>
-                        {country.label}
-                      </MenuItem>
-                    ))}
-                  </Select>
-                  {formik.touched.country && formik.errors.country && (
-                    <FormHelperText>{formik.errors.country}</FormHelperText>
-                  )}
-                </FormControl>
-                {/* <Field
-                  component={Select}
-                  id="country"
-                  name="country"
-                  InputLabelProps={{
-                    style: { color: '#091527' }
-                  }}
-                  style={{ marginTop: '10px' }}
-                  IconComponent={() => <ExpandMoreIcon />}
-                  label="Select"
-                  variant="outlined"
-                  fullWidth
-                  SelectProps={{
-                    MenuProps: menuProps
-                  }}
-                  InputProps={{ notched: false }}
-                >
-                  {countryList.map((country) => (
-                    <MenuItem key={country.label} value={country.value}>
-                      {country.label}
-                    </MenuItem>
-                  ))}
-                </Field> */}
-              </Grid>
-              <Grid item md={7} lg={8} xs={6} sm={5}>
-                <TextField
-                  id="phonenumber"
-                  name="phonenumber"
-                  label="Mobile Phone"
-                  variant="outlined"
-                  fullWidth
-                  className={classes.TextFieldMobile}
-                  value={formik.values.phonenumber}
-                  onChange={formik.handleChange}
-                  error={
-                    formik.touched.phonenumber &&
-                    Boolean(formik.errors.phonenumber)
-                  }
-                  InputLabelProps={{
-                    style: { color: '#091527' }
-                  }}
-                  // InputLabelProps={{
-                  //   style: { color: '#091527' }
-                  // }}
-                  // InputProps={{ notched: false }}
-                />
-                {formik.touched.phonenumber && formik.errors.phonenumber && (
-                  <FormHelperText style={{ color: 'red' }}>
-                    {formik.errors.phonenumber}
-                  </FormHelperText>
-                )}
-              </Grid>
-            </Grid>
-            <Grid item md={6} sm={3} xs={12}>
-              <TextField
-                id="comments"
-                name="comments"
-                label="Your Comments"
-                multiline
-                rows={10}
-                variant="outlined"
-                fullWidth
-                className={classes.TextFieldComment}
-                InputLabelProps={{
-                  style: { color: '#091527' }
-                }}
-                // InputProps={{ notched: false }}
-                // InputLabelProps={{
-                //   style: { color: '#091527' }
-                // }}
-                value={formik.values.comments}
-                onChange={formik.handleChange}
-                error={
-                  formik.touched.comments && Boolean(formik.errors.comments)
-                }
-              />{' '}
-              {formik.touched.comments && formik.errors.comments && (
-                <FormHelperText style={{ color: 'red' }}>
-                  {formik.errors.comments}
-                </FormHelperText>
-              )}
-            </Grid>
-          </Grid>
+      <Container className={classes.Container}>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField
+            id="outlined-read-only-input"
+            label="Full Name"
+            value={enquiry?.fullName}
+            fullWidth
+            defaultValue="Hello World"
+            InputProps={{
+              readOnly: true
+            }}
+            variant="outlined"
+          />
+          <br />
+          <br />
+          <TextField
+            id="outlined-read-only-input"
+            label="Email"
+            value={enquiry?.email}
+            fullWidth
+            defaultValue="Hello World"
+            InputProps={{
+              readOnly: true
+            }}
+            variant="outlined"
+          />{' '}
+          <br />
+          <br />
+          <TextField
+            id="outlined-read-only-input"
+            label="phoneNumber"
+            fullWidth
+            value={enquiry?.phoneNumber}
+            defaultValue="Hello World"
+            InputProps={{
+              readOnly: true
+            }}
+            variant="outlined"
+          />{' '}
+          <br />
+          <br />
+          <TextField
+            id="outlined-read-only-input"
+            label="Desination"
+            fullWidth
+            value={enquiry?.preferredDestination}
+            defaultValue="Hello World"
+            InputProps={{
+              readOnly: true
+            }}
+            variant="outlined"
+          />{' '}
+          <br />
+          <br />
+          <TextField
+            id="outlined-read-only-input"
+            label="Comments"
+            value={enquiry?.comment}
+            fullWidth
+            defaultValue="Hello World"
+            InputProps={{
+              readOnly: true
+            }}
+            variant="outlined"
+          />{' '}
+          <br />
+          <br />
+          <TextField
+            id="outlined-read-only-input"
+            label="Title"
+            fullWidth
+            value={enquiry?.title}
+            defaultValue="Hello World"
+            InputProps={{
+              readOnly: true
+            }}
+            variant="outlined"
+          />{' '}
+          <br />
+          <br />
+          <TextField
+            id="outlined-read-only-input"
+            label="Comments"
+            defaultValue="Hello World"
+            InputProps={{
+              readOnly: true
+            }}
+            variant="outlined"
+            multiline
+            rows={10}
+            fullWidth
+            className={classes.TextFieldComment}
+            InputLabelProps={{
+              style: { color: '#091527' }
+            }}
+            // InputProps={{ notched: false }}
+            // InputLabelProps={{
+            //   style: { color: '#091527' }
+            // }}
+            value={enquiry?.comment}
+          />{' '}
         </form>
       </Container>
     </Box>
